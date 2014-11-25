849ee8ce6bce857d304a2748b39587421f95249a
Matt Hinchliffe <matt@maketea.co.uk>
2014-11-25 16:07:20 +0000

    Add line to predictions

--- app/component/predictions.jsx
+++ app/component/predictions.jsx
@@ -112,6 +112,7 @@ var Trains = React.createClass({
         <tr className="trains__arrival" key={"train-" + train.vehicleId}>
           <td>{timeTo === "0:00" ? "-" : timeTo}</td>
           <td>{train.towards}</td>
+          <td>{train.lineName}</td>
           <td>{train.currentLocation}</td>
         </tr>
       );
@@ -123,6 +124,7 @@ var Trains = React.createClass({
           <tr>
             <th>Time</th>
             <th>Destination</th>
+            <th>Line</th>
             <th>Current location</th>
           </tr>
         </thead>
